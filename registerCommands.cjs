ef5a37a3302ef4c90ef2a60e4e32be42e6ba8250
root <root@Suomi24.laaksonen.eu>
2022-06-07 09:40:46 +0000

    add slash command registering

---
+++ registerCommands.cjs
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require('dotenv').config()
+
+const commands = [
+	new SlashCommandBuilder().setName('tukari').setDescription('Lista paikalla olevista'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
